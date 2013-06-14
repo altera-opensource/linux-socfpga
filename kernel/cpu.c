1e15fdcb6888a63934685fa3181a262c1bd2b6bb
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2013-06-14 17:16:35 +0200

    kernel/hotplug: restore original cpu mask oncpu/down
    If a task which is allowed to run only on CPU X puts CPU Y down then it
    will be allowed on all CPUs but the on CPU Y after it comes back from
    kernel. This patch ensures that we don't lose the initial setting unless
    the CPU the task is running is going down.
    
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -1166,6 +1166,7 @@ static int __ref _cpu_down(unsigned int cpu, int tasks_frozen,
 	bool hasdied = false;
 	int mycpu;
 	cpumask_var_t cpumask;
+	cpumask_var_t cpumask_org;
 
 	if (num_online_cpus() == 1)
 		return -EBUSY;
@@ -1176,6 +1177,12 @@ static int __ref _cpu_down(unsigned int cpu, int tasks_frozen,
 	/* Move the downtaker off the unplug cpu */
 	if (!alloc_cpumask_var(&cpumask, GFP_KERNEL))
 		return -ENOMEM;
+	if (!alloc_cpumask_var(&cpumask_org, GFP_KERNEL))  {
+		free_cpumask_var(cpumask);
+		return -ENOMEM;
+	}
+
+	cpumask_copy(cpumask_org, tsk_cpus_allowed(current));
 	cpumask_andnot(cpumask, cpu_online_mask, cpumask_of(cpu));
 	set_cpus_allowed_ptr(current, cpumask);
 	free_cpumask_var(cpumask);
@@ -1184,7 +1191,8 @@ static int __ref _cpu_down(unsigned int cpu, int tasks_frozen,
 	if (mycpu == cpu) {
 		printk(KERN_ERR "Yuck! Still on unplug CPU\n!");
 		migrate_enable();
-		return -EBUSY;
+		ret = -EBUSY;
+		goto restore_cpus;
 	}
 
 	cpu_hotplug_begin();
@@ -1238,6 +1246,9 @@ out_cancel:
 	/* This post dead nonsense must die */
 	if (!ret && hasdied)
 		cpu_notify_nofail(CPU_POST_DEAD, cpu);
+restore_cpus:
+	set_cpus_allowed_ptr(current, cpumask_org);
+	free_cpumask_var(cpumask_org);
 	return ret;
 }
 
