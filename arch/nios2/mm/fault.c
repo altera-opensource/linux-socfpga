31372e2d459d623337a41ea39d8498895237ca22
Ley Foon Tan <lftan@altera.com>
2015-03-16 15:35:11 +0800

    nios2: mm: do not invoke OOM killer on kernel fault OOM
    Follow commit 871341023c771ad.
    
    Kernel faults are expected to handle OOM conditions gracefully (gup,
    uaccess etc.), so they should never invoke the OOM killer.  Reserve
    this for faults triggered in user context when it is the only option.
    
    Signed-off-by: Ley Foon Tan <lftan@altera.com>

--- arch/nios2/mm/fault.c
+++ arch/nios2/mm/fault.c
@@ -123,7 +123,6 @@ good_area:
 		break;
 	}
 
-survive:
 	/*
 	 * If for any reason at all we couldn't handle the fault,
 	 * make sure we exit gracefully rather than endlessly redo
@@ -216,15 +215,10 @@ no_context:
  */
 out_of_memory:
 	up_read(&mm->mmap_sem);
-	if (is_global_init(tsk)) {
-		yield();
-		down_read(&mm->mmap_sem);
-		goto survive;
-	}
-	pr_info("VM: killing process %s\n", tsk->comm);
-	if (user_mode(regs))
-		do_group_exit(SIGKILL);
-	goto no_context;
+	if (!user_mode(regs))
+		goto no_context;
+	pagefault_out_of_memory();
+	return;
 
 do_sigbus:
 	up_read(&mm->mmap_sem);
