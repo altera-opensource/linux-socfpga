566be53df89df8ed737d914da67b9655e71b7069
Niklas Rosencrantz <niklas@montao.com.br>
2017-04-06 14:12:20 +0200

    Update setup.c

--- arch/nios2/kernel/setup.c
+++ arch/nios2/kernel/setup.c
@@ -7,7 +7,7 @@
  *
  * This file is subject to the terms and conditions of the GNU General Public
  * License. See the file "COPYING" in the main directory of this archive
- * for more details.!
+ * for more details.
  */
 
 #include <linux/export.h>
