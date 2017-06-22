2d8a1c7a37804f11144039c42d6190c85b8b9ecb
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2017-06-22 17:53:34 +0200

    kernel/locking: use an exclusive wait_q for sleepers
    If a task is queued as a sleeper for a wakeup and never goes to
    schedule() (because it just obtained the lock) then it will receive a
    spurious wake up which is not "bad", it is considered. Until that wake
    up happens this task can no be enqueued for any wake ups handled by the
    WAKE_Q infrastructure (because a task can only be enqueued once). This
    wouldn't be bad if we would use the same wakeup mechanism for the wake
    up of sleepers as we do for "normal" wake ups. But we don't…
    
    So.
       T1			T2		T3
       spin_lock(x)				spin_unlock(x);
       					wake_q_add_sleeper(q1, T1)
       spin_unlock(x)
       set_state(TASK_INTERRUPTIBLE)
       if (!condition)
    	schedule()
    			condition = true
    			wake_q_add(q2, T1)
    	                // T1 not added, still enqueued
    			wake_up_q(q2)
    					wake_up_q_sleeper(q1)
    					// T1 not woken up, wrong task state
    
    In order to solve this race this patch adds a wake_q_node for the
    sleeper case.
    
    Reported-by: Mike Galbraith <efault@gmx.de>
    Cc: stable-rt@vger.kernel.org
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- kernel/locking/rtmutex.c
+++ kernel/locking/rtmutex.c
@@ -1506,7 +1506,7 @@ static void mark_wakeup_next_waiter(struct wake_q_head *wake_q,
 	 */
 	preempt_disable();
 	if (waiter->savestate)
-		wake_q_add(wake_sleeper_q, waiter->task);
+		wake_q_add_sleeper(wake_sleeper_q, waiter->task);
 	else
 		wake_q_add(wake_q, waiter->task);
 	raw_spin_unlock(&current->pi_lock);
