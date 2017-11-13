a4c2854e1c96d647c8744ff61a7ff8e2bfe0a775
Thomas Gleixner <tglx@linutronix.de>
2017-11-13 20:23:44 +0100

    timers: Use static keys for migrate_enable and nohz_active
    The migration_enabled and nohz_active will be later moved into the bitfield. In
    the bitfield a change to one bit causes RMW operation and without holding a
    lock it might happen that a concurrent change on a second CPU might cause the
    loss of the an update.
    To avoid that and since both fields are changed to static_branch.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- kernel/time/tick-internal.h
+++ kernel/time/tick-internal.h
@@ -150,14 +150,25 @@ static inline void tick_nohz_init(void) { }
 
 #ifdef CONFIG_NO_HZ_COMMON
 extern unsigned long tick_nohz_active;
-#else
-#define tick_nohz_active (0)
-#endif
+extern void timers_update_nohz(void);
+extern struct static_key_false timers_nohz_active;
 
-#if defined(CONFIG_SMP) && defined(CONFIG_NO_HZ_COMMON)
-extern void timers_update_migration(bool update_nohz);
+static inline bool is_timers_nohz_active(void)
+{
+	return static_branch_unlikely(&timers_nohz_active);
+}
+
+#ifdef CONFIG_SMP
+extern struct static_key_false timers_migration_enabled;
+#endif
 #else
-static inline void timers_update_migration(bool update_nohz) { }
+static inline void timers_update_nohz(void) { }
+#define tick_nohz_active (0)
+
+static inline bool is_timers_nohz_active(void)
+{
+	return false;
+}
 #endif
 
 DECLARE_PER_CPU(struct hrtimer_cpu_base, hrtimer_bases);
