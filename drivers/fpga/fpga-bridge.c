7df2a7d49e75ae160105ef098ac4918338733405
Alan Tull <atull@kernel.org>
2018-02-20 09:56:58 -0600

    FogBugz #536771: fpga: bridge: fix klocwork sprintf complaint
    Klocwork give the following complaints:
    
    /home/yves/linux/intel/linux-socfpga/drivers/fpga/fpga-bridge.c:243 --
    -- SV.BANNED.REQUIRED.SPRINTF (4:Review) Analyze Function 'sprintf' is
    deprecated.  Replace with more secure equivalent like 'sprintf_s', add
    missing logic, or re-architect.
    
    /home/yves/linux/intel/linux-socfpga/drivers/fpga/fpga-bridge.c:255 --
    -- SV.BANNED.REQUIRED.SPRINTF (4:Review) Analyze Function 'sprintf' is
    deprecated.  Replace with more secure equivalent like 'sprintf_s', add
    missing logic, or re-architect.
    
    Remedy these by using scnprintf rather than sprintf.
    
    Signed-off-by: Alan Tull <atull@kernel.org>
    ---
    v2: s/klockwork/klocwork/ and s/bringe/bridge/ in header

--- drivers/fpga/fpga-bridge.c
+++ drivers/fpga/fpga-bridge.c
@@ -286,7 +286,7 @@ static ssize_t name_show(struct device *dev,
 {
 	struct fpga_bridge *bridge = to_fpga_bridge(dev);
 
-	return sprintf(buf, "%s\n", bridge->name);
+	return scnprintf(buf, PAGE_SIZE, "%s\n", bridge->name);
 }
 
 static ssize_t state_show(struct device *dev,
