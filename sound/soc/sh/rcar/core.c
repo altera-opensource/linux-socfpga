3e36aad1f07874423dcc38603e81892cd6368726
Arnd Bergmann <arnd@arndb.de>
2018-03-28 16:53:10 +0200

    ASoC: rsnd: mark PM functions __maybe_unused
    The suspend/resume callbacks are now optional, leading to a warning
    when they are unused:
    
    sound/soc/sh/rcar/core.c:1548:12: error: 'rsnd_resume' defined but not used [-Werror=unused-function]
     static int rsnd_resume(struct device *dev)
                ^~~~~~~~~~~
    sound/soc/sh/rcar/core.c:1539:12: error: 'rsnd_suspend' defined but not used [-Werror=unused-function]
     static int rsnd_suspend(struct device *dev)
    
    This marks the as __maybe_unused to avoid the warning.
    
    Fixes: f8a9a29c4fe9 ("ASoC: rsnd: set pm_ops in hibernate-compatible way")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit 6f5427039c33e149b711c0f973fcac7f6875b768)
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>

--- sound/soc/sh/rcar/core.c
+++ sound/soc/sh/rcar/core.c
@@ -1569,7 +1569,7 @@ static int rsnd_remove(struct platform_device *pdev)
 	return ret;
 }
 
-static int rsnd_suspend(struct device *dev)
+static int __maybe_unused rsnd_suspend(struct device *dev)
 {
 	struct rsnd_priv *priv = dev_get_drvdata(dev);
 
@@ -1578,7 +1578,7 @@ static int rsnd_suspend(struct device *dev)
 	return 0;
 }
 
-static int rsnd_resume(struct device *dev)
+static int __maybe_unused rsnd_resume(struct device *dev)
 {
 	struct rsnd_priv *priv = dev_get_drvdata(dev);
 
