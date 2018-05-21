0129d25f146ec76756ac5f33859a1bc9bf3c42bf
Mathias Nyman <mathias.nyman@linux.intel.com>
2018-05-21 16:39:53 +0300

    xhci: set hcd pointers for xhci usb2 and usb3 roothub structures
    Allows us to know the correct hcd a xhci roothub and its ports
    belong to.
    
    Signed-off-by: Mathias Nyman <mathias.nyman@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 9ea95ecc7ffd338a62972ddd6b0ac46bdc5e7d49)
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>

--- drivers/usb/host/xhci.c
+++ drivers/usb/host/xhci.c
@@ -4958,6 +4958,7 @@ int xhci_gen_setup(struct usb_hcd *hcd, xhci_get_quirks_t get_quirks)
 
 	if (usb_hcd_is_primary_hcd(hcd)) {
 		xhci->main_hcd = hcd;
+		xhci->usb2_rhub.hcd = hcd;
 		/* Mark the first roothub as being USB 2.0.
 		 * The xHCI driver will register the USB 3.0 roothub.
 		 */
@@ -4983,6 +4984,7 @@ int xhci_gen_setup(struct usb_hcd *hcd, xhci_get_quirks_t get_quirks)
 			  minor_rev,
 			  minor_rev ? "Enhanced" : "");
 
+		xhci->usb3_rhub.hcd = hcd;
 		/* xHCI private pointer was set in xhci_pci_probe for the second
 		 * registered roothub.
 		 */
