79f9717af336849789ff62bab08a5c05d78ae1f8
Scott Wood <swood@redhat.com>
2019-01-04 15:33:21 -0500

    locking/rt-mutex: Flush block plug on __down_read()
    [ Upstream commit 0d5d32425a0cab4a282dd79fc953d76a3ab17072 ]
    
    __down_read() bypasses the rtmutex frontend to call
    rt_mutex_slowlock_locked() directly, and thus it needs to call
    blk_schedule_flush_flug() itself.
    
    Cc: stable-rt@vger.kernel.org
    Signed-off-by: Scott Wood <swood@redhat.com>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Tom Zanussi <tom.zanussi@linux.intel.com>
    
     Conflicts:
    	kernel/locking/rwsem-rt.c

--- kernel/locking/rwsem-rt.c
+++ kernel/locking/rwsem-rt.c
@@ -1,5 +1,6 @@
 /*
  */
+#include <linux/blkdev.h>
 #include <linux/rwsem.h>
 #include <linux/sched/debug.h>
 #include <linux/sched/signal.h>
@@ -86,6 +87,14 @@ void __sched __down_read(struct rw_semaphore *sem)
 
 	if (__down_read_trylock(sem))
 		return;
+	/*
+	 * If rt_mutex blocks, the function sched_submit_work will not call
+	 * blk_schedule_flush_plug (because tsk_is_pi_blocked would be true).
+	 * We must call blk_schedule_flush_plug here, if we don't call it,
+	 * a deadlock in I/O may happen.
+	 */
+	if (unlikely(blk_needs_flush_plug(current)))
+		blk_schedule_flush_plug(current);
 
 	might_sleep();
 	raw_spin_lock_irq(&m->wait_lock);
