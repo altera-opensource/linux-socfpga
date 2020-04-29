ba9e1527d60f177fa33467f24489b45b56962a21
Dalon Westergreen <dalon.westergreen@intel.com>
2020-04-29 17:46:57 +0800

    net: eth: altera: Move common functions to altera_utils
    Move request_and_map and other shared functions to altera_utils. This
    is the first step to moving common code out of tse specific code so
    that it can be shared with future altera ethernet ip.
    
    Signed-off-by: Dalon Westergreen <dalon.westergreen@intel.com>
    Signed-off-by: Joyce Ooi <joyce.ooi@intel.com>

--- drivers/net/ethernet/altera/altera_tse_main.c
+++ drivers/net/ethernet/altera/altera_tse_main.c
@@ -23,7 +23,6 @@
 #include <linux/if_vlan.h>
 #include <linux/init.h>
 #include <linux/interrupt.h>
-#include <linux/io.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/mii.h>
@@ -33,7 +32,7 @@
 #include <linux/of_net.h>
 #include <linux/of_platform.h>
 #include <linux/phy.h>
-#include <linux/platform_device.h>
+#include <linux/ptp_classify.h>
 #include <linux/skbuff.h>
 #include <asm/cacheflush.h>
 
