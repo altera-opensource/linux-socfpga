341a2391e0115a62dc458df8f134be4c41c4a956
radu bacrau <radu.bacrau@intel.com>
2020-08-27 10:04:47 -0500

    [PATCH] HSD #14011791668: firmware: stratix10-svc: extend svc to  support RSU feature
    Extend Intel Stratix10 service layer driver to support new RSU
    DCMF status reporting.
    
    The status of each DCMF is reported. The currently used DCMF is used as
    reference, while the other three are compared against it to determine if
    they are corrupted.
    
    DCMF = Decision Configuration Management Firmware
    RSU = Remote System Update
    
    Signed-off-by: Radu Bacrau <radu.bacrau@intel.com>

--- drivers/firmware/stratix10-svc.c
+++ drivers/firmware/stratix10-svc.c
@@ -340,6 +340,7 @@ static void svc_thread_recv_status_ok(struct stratix10_svc_data *p_data,
 	case COMMAND_RSU_RETRY:
 	case COMMAND_RSU_MAX_RETRY:
 	case COMMAND_FIRMWARE_VERSION:
+	case COMMAND_RSU_DCMF_STATUS:
 		cb_data->status = BIT(SVC_STATUS_OK);
 		cb_data->kaddr1 = &res.a1;
 		break;
@@ -469,6 +470,10 @@ static int svc_normal_to_secure_thread(void *data)
 			break;
 		case COMMAND_FIRMWARE_VERSION:
 			a0 = INTEL_SIP_SMC_FIRMWARE_VERSION;
+			a1 = 0;
+			a2 = 0;
+		case COMMAND_RSU_DCMF_STATUS:
+			a0 = INTEL_SIP_SMC_RSU_DCMF_STATUS;
 			a1 = 0;
 			a2 = 0;
 			break;
@@ -608,12 +613,10 @@ static int svc_normal_to_secure_thread(void *data)
 
 			/*
 			 * be compatible with older version firmware which
-			 * doesn't support RSU notify or retry
+			 * doesn't support newer RSU commands
 			 */
-			if ((pdata->command == COMMAND_RSU_RETRY) ||
-			    (pdata->command == COMMAND_RSU_MAX_RETRY) ||
-			    (pdata->command == COMMAND_RSU_NOTIFY) ||
-			    (pdata->command == COMMAND_FIRMWARE_VERSION)) {
+			if ((pdata->command != COMMAND_RSU_UPDATE) &&
+			    (pdata->command != COMMAND_RSU_STATUS)) {
 				cbdata->status =
 					BIT(SVC_STATUS_NO_SUPPORT);
 				cbdata->kaddr1 = NULL;
