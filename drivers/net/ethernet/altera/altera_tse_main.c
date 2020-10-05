7d1ccc119f08cec66556c3c22c8c1868ccd77f1c
Dinh Nguyen <dinh.nguyen@intel.com>
2020-10-05 10:00:33 -0500

    Revert "net: eth: altera: Move common functions to altera_utils"
    This reverts commit 4fa59a4ecba92d52e3642f19811226fbb0ec972e.
    There was an error during the original cherry-pick that had the
    function request_and_map() declared twice.
    
    Signed-off-by: Dinh Nguyen <dinh.nguyen@intel.com>

--- drivers/net/ethernet/altera/altera_tse_main.c
+++ drivers/net/ethernet/altera/altera_tse_main.c
@@ -25,6 +25,7 @@
 #include <linux/if_vlan.h>
 #include <linux/init.h>
 #include <linux/interrupt.h>
+#include <linux/io.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/mii.h>
@@ -35,7 +36,7 @@
 #include <linux/of_net.h>
 #include <linux/of_platform.h>
 #include <linux/phy.h>
-#include <linux/ptp_classify.h>
+#include <linux/platform_device.h>
 #include <linux/skbuff.h>
 #include <asm/cacheflush.h>
 
