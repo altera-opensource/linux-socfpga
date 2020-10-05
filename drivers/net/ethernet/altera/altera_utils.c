fb5c1a36b3501521c5c4747eb35ce916f7024432
Dinh Nguyen <dinh.nguyen@intel.com>
2020-10-05 10:00:33 -0500

    Revert "net: eth: altera: Move common functions to altera_utils"
    This reverts commit 4fa59a4ecba92d52e3642f19811226fbb0ec972e.
    There was an error during the original cherry-pick that had the
    function request_and_map() declared twice.
    
    Signed-off-by: Dinh Nguyen <dinh.nguyen@intel.com>

--- drivers/net/ethernet/altera/altera_utils.c
+++ drivers/net/ethernet/altera/altera_utils.c
@@ -32,32 +32,3 @@ int tse_bit_is_clear(void __iomem *ioaddr, size_t offs, u32 bit_mask)
 	u32 value = csrrd32(ioaddr, offs);
 	return (value & bit_mask) ? 0 : 1;
 }
-
-int request_and_map(struct platform_device *pdev, const char *name,
-		    struct resource **res, void __iomem **ptr)
-{
-	struct resource *region;
-	struct device *device = &pdev->dev;
-
-	*res = platform_get_resource_byname(pdev, IORESOURCE_MEM, name);
-	if (!*res) {
-		dev_err(device, "resource %s not defined\n", name);
-		return -ENODEV;
-	}
-
-	region = devm_request_mem_region(device, (*res)->start,
-					 resource_size(*res), dev_name(device));
-	if (!region) {
-		dev_err(device, "unable to request %s\n", name);
-		return -EBUSY;
-	}
-
-	*ptr = devm_ioremap(device, region->start,
-			    resource_size(region));
-	if (!*ptr) {
-		dev_err(device, "ioremap of %s failed!", name);
-		return -ENOMEM;
-	}
-
-	return 0;
-}
