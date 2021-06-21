c778fe37b9a57f77689c9e696d1d5f7914a96147
Richard Gong <richard.gong@intel.com>
2021-06-21 16:41:54 -0500

    firmware: stratix10-svc: update for exporting the service key object
    Update to support exporting the service key object.
    
    Signed-off-by: Richard Gong <richard.gong@intel.com>

--- include/linux/firmware/intel/stratix10-svc-client.h
+++ include/linux/firmware/intel/stratix10-svc-client.h
@@ -180,6 +180,9 @@ struct stratix10_svc_chan;
  *
  * @COMMAND_FCS_CRYPTO_IMPORT_KEY: import the crypto service key object,
  * return status is SVC_STATUS_OK or SVC_STATUS_ERROR
+ *
+ * @COMMAND_FCS_CRYPTO_EXPORT_KEY: export the crypto service key object,
+ * return status is SVC_STATUS_OK or SVC_STATUS_ERROR
  */
 enum stratix10_svc_command_code {
 	/* for FPGA */
@@ -222,6 +225,7 @@ enum stratix10_svc_command_code {
 	COMMAND_FCS_CRYPTO_OPEN_SESSION = 50,
 	COMMAND_FCS_CRYPTO_CLOSE_SESSION,
 	COMMAND_FCS_CRYPTO_IMPORT_KEY,
+	COMMAND_FCS_CRYPTO_EXPORT_KEY,
 };
 
 /**
