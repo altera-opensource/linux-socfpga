044dc9a421146811ce7350336dd5286296901b30
Richard Gong <richard.gong@intel.com>
2021-06-22 12:24:41 -0500

    firmware: stratix10-svc: update for getting the service key info
    Update to support getting the service key info.
    
    Signed-off-by: Richard Gong <richard.gong@intel.com>
    Signed-off-by: Siew Chin Lim <elly.siew.chin.lim@intel.com>

--- include/linux/firmware/intel/stratix10-svc-client.h
+++ include/linux/firmware/intel/stratix10-svc-client.h
@@ -183,6 +183,12 @@ struct stratix10_svc_chan;
  *
  * @COMMAND_FCS_CRYPTO_EXPORT_KEY: export the crypto service key object,
  * return status is SVC_STATUS_OK or SVC_STATUS_ERROR
+ *
+ * @COMMAND_FCS_CRYPTO_REMOVE_KEY: remove the crypto service key object
+ * from the device, return status is SVC_STATUS_OK or SVC_STATUS_ERROR
+ *
+ * @COMMAND_FCS_CRYPTO_GET_KEY_INFO: get the crypto service key object
+ * info, return status is SVC_STATUS_OK or SVC_STATUS_ERROR
  */
 enum stratix10_svc_command_code {
 	/* for FPGA */
@@ -226,6 +232,8 @@ enum stratix10_svc_command_code {
 	COMMAND_FCS_CRYPTO_CLOSE_SESSION,
 	COMMAND_FCS_CRYPTO_IMPORT_KEY,
 	COMMAND_FCS_CRYPTO_EXPORT_KEY,
+	COMMAND_FCS_CRYPTO_REMOVE_KEY,
+	COMMAND_FCS_CRYPTO_GET_KEY_INFO,
 };
 
 /**
