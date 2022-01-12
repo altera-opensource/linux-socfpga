0097cfecc32706d4089fa296a4c2da03c9ff5309
Tham, Mun Yew <mun.yew.tham@intel.com>
2022-01-12 14:46:47 +0800

    HSD #14014656595: net: phy: add PHY_INTERFACE_MODE_25GKR
    Add PHY_INTERFACE_MODE_25GKR for 25 Gigabit Ethernet
    Signed-off-by: Tham, Mun Yew <mun.yew.tham@intel.com>

--- include/linux/phy.h
+++ include/linux/phy.h
@@ -144,6 +144,8 @@ typedef enum {
 	PHY_INTERFACE_MODE_USXGMII,
 	/* 10GBASE-KR - with Clause 73 AN */
 	PHY_INTERFACE_MODE_10GKR,
+	/* 25GBASE-KR */
+	PHY_INTERFACE_MODE_25GKR,
 	PHY_INTERFACE_MODE_MAX,
 } phy_interface_t;
 
@@ -217,6 +219,8 @@ static inline const char *phy_modes(phy_interface_t interface)
 		return "usxgmii";
 	case PHY_INTERFACE_MODE_10GKR:
 		return "10gbase-kr";
+	case PHY_INTERFACE_MODE_25GKR:
+		return "25gbase-kr";
 	default:
 		return "unknown";
 	}
