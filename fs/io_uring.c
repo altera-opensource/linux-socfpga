aba73a3db8f67eb069a63685dfbc2cc8ff77d9e6
Pavel Begunkov <asml.silence@gmail.com>
2022-08-29 14:30:16 +0100

    io_uring: kill poll linking optimisation
    [ upstream commmit ab1dab960b8352cee082db0f8a54dc92a948bfd7 ]
    
    With IORING_FEAT_FAST_POLL in place, io_put_req_find_next() for poll
    requests doesn't make much sense, and in any case re-adding it
    shouldn't be a problem considering batching in tctx_task_work(). We can
    remove it.
    
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Link: https://lore.kernel.org/r/15699682bf81610ec901d4e79d6da64baa9f70be.1639605189.git.asml.silence@gmail.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    [pavel: backport]
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -5450,7 +5450,6 @@ static inline bool io_poll_complete(struct io_kiocb *req, __poll_t mask)
 static void io_poll_task_func(struct io_kiocb *req, bool *locked)
 {
 	struct io_ring_ctx *ctx = req->ctx;
-	struct io_kiocb *nxt;
 
 	if (io_poll_rewait(req, &req->poll)) {
 		spin_unlock(&ctx->completion_lock);
@@ -5474,11 +5473,8 @@ static void io_poll_task_func(struct io_kiocb *req, bool *locked)
 		spin_unlock(&ctx->completion_lock);
 		io_cqring_ev_posted(ctx);
 
-		if (done) {
-			nxt = io_put_req_find_next(req);
-			if (nxt)
-				io_req_task_submit(nxt, locked);
-		}
+		if (done)
+			io_put_req(req);
 	}
 }
 
