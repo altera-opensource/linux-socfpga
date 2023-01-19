e24c7f18c4ccd7717370ca9df83e3f2476a46181
KrishnaSimmadhari <91877513+KrishnaSimmadhari@users.noreply.github.com>
2023-01-19 18:09:50 +0530

    Simmadha/qsfp mem controller driver (#20)
    fpga: QSFP controller subsystem driver for Intel FPGAs
    
    Qsfp controller driver to include platform and DFL interface driver. DFL driver is used
    for the host which accesses the QSFP controller subsystem using the PCIe bus and
    the platform driver is used where the subsystem is accessed through the AXI/AXI-Lite bus.
    
    Signed-off-by: Krishna Kumar S R simmadhari.ramadass.krishna.kumar@intel.com

---
+++ drivers/net/phy/qsfp-mem-core.c
@@ -0,0 +1,177 @@
+// SPDX-License-Identifier: GPL
+
+/* Intel(R) Memory based QSFP driver.
+ *
+ * Copyright (C) 2020,2022 Intel Corporation. All rights reserved.
+ */
+
+#include <linux/bitfield.h>
+#include <linux/etherdevice.h>
+#include <linux/ethtool.h>
+#include <linux/i2c.h>
+#include <linux/io-64-nonatomic-lo-hi.h>
+#include <linux/module.h>
+#include <linux/netdevice.h>
+#include <linux/regmap.h>
+#include <linux/uaccess.h>
+#include <linux/phy/qsfp-mem.h>
+
+#define CONF_OFF	0x20
+#define CONF_RST_MOD	BIT(0)
+#define CONF_RST_CON	BIT(1)
+#define CONF_MOD_SEL	BIT(2)
+#define CONF_LOW_POW	BIT(3)
+#define CONF_POLL_EN	BIT(4)
+
+#define STAT_OFF	0x28
+#define MODPRSL         BIT(0)
+#define DELAY_REG       0x38
+#define DELAY_VALUE       0xffffff
+
+#define I2C_CTRL        0x48
+#define I2C_CTRL_EN	BIT(0)
+#define I2C_CTRL_BSP	BIT(1)
+#define I2C_CTRL_FIFO  GENMASK(3, 2)
+#define I2C_CTRL_FIFO_NOT_FULL 3
+
+#define I2C_ISER	0x4c
+#define I2C_ISER_TXRDY	BIT(0)
+#define I2C_ISER_RXRDY	BIT(1)
+#define I2C_SCL_LOW	0x60
+#define COUNT_PERIOD_LOW 0x82
+#define I2C_SCL_HIGH	0x64
+#define COUNT_PERIOD_HIGH 0x3c
+#define I2C_SDA_HOLD	0x68
+#define COUNT_PERIOD_HOLD 0x28
+
+#define QSFP_SHADOW_CSRS_BASE_OFF	0x100
+#define QSFP_SHADOW_CSRS_BASE_END	0x3f8
+
+#define DELAY_US 1000
+
+#define QSFP_CHECK_TIME 500
+
+static const struct regmap_range qsfp_mem_regmap_range[] = {
+	regmap_reg_range(CONF_OFF, STAT_OFF),
+	regmap_reg_range(QSFP_SHADOW_CSRS_BASE_OFF, QSFP_SHADOW_CSRS_BASE_END),
+};
+
+static const struct regmap_access_table qsfp_mem_access_table = {
+	.yes_ranges	= qsfp_mem_regmap_range,
+	.n_yes_ranges	= ARRAY_SIZE(qsfp_mem_regmap_range),
+};
+
+static void qsfp_init_i2c(struct qsfp *qsfp)
+{
+	writel(I2C_ISER_TXRDY | I2C_ISER_RXRDY, qsfp->base + I2C_ISER);
+	writel(COUNT_PERIOD_LOW, qsfp->base + I2C_SCL_LOW);
+	writel(COUNT_PERIOD_HIGH, qsfp->base + I2C_SCL_HIGH);
+	writel(COUNT_PERIOD_HOLD, qsfp->base + I2C_SDA_HOLD);
+
+	writel(FIELD_PREP(I2C_CTRL_FIFO, I2C_CTRL_FIFO_NOT_FULL) |
+			I2C_CTRL_EN | I2C_CTRL_BSP, qsfp->base + I2C_CTRL);
+}
+
+static const struct regmap_config mmio_cfg = {
+	.reg_bits = 64,
+	.reg_stride = 8,
+	.val_bits = 64,
+	.fast_io = true,
+	.rd_table = &qsfp_mem_access_table,
+	.max_register = QSFP_SHADOW_CSRS_BASE_END,
+};
+
+static void qsfp_init(struct qsfp *qsfp)
+{
+	writeq(CONF_RST_MOD | CONF_RST_CON | CONF_MOD_SEL, qsfp->base + CONF_OFF);
+	udelay(DELAY_US);
+	writeq(CONF_MOD_SEL, qsfp->base + CONF_OFF);
+	udelay(DELAY_US);
+
+	qsfp_init_i2c(qsfp);
+
+	udelay(DELAY_US);
+	writeq(DELAY_VALUE, qsfp->base + DELAY_REG);
+
+	writeq(CONF_POLL_EN | CONF_MOD_SEL, qsfp->base + CONF_OFF);
+	udelay(DELAY_US);
+}
+
+int check_qsfp_plugin(struct qsfp *qsfp)
+{
+	u64 status;
+
+	status = readq(qsfp->base + STAT_OFF);
+
+	return (!(status & MODPRSL));
+}
+EXPORT_SYMBOL_GPL(check_qsfp_plugin);
+
+u32 qsfp_connected_show(struct qsfp *qsfp)
+{
+	u32 plugin;
+
+	mutex_lock(&qsfp->lock);
+	plugin = check_qsfp_plugin(qsfp) && (qsfp->init == QSFP_INIT_DONE);
+	mutex_unlock(&qsfp->lock);
+
+	return plugin;
+}
+EXPORT_SYMBOL_GPL(qsfp_connected_show);
+
+void qsfp_check_hotplug(struct work_struct *work)
+{
+	struct delayed_work *dwork;
+	struct qsfp *qsfp;
+	u64 status;
+
+	dwork = to_delayed_work(work);
+	qsfp = container_of(dwork, struct qsfp, dwork);
+
+	mutex_lock(&qsfp->lock);
+
+	status = readq(qsfp->base + STAT_OFF);
+	dev_dbg(qsfp->dev, "qsfp status 0x%llx\n", status);
+
+	if (check_qsfp_plugin(qsfp) && qsfp->init == QSFP_INIT_RESET) {
+		dev_info(qsfp->dev, "detected QSFP plugin\n");
+		qsfp_init(qsfp);
+		WRITE_ONCE(qsfp->init, QSFP_INIT_DONE);
+	} else if (!check_qsfp_plugin(qsfp) &&
+		   qsfp->init == QSFP_INIT_DONE) {
+		dev_info(qsfp->dev, "detected QSFP unplugin\n");
+		WRITE_ONCE(qsfp->init, QSFP_INIT_RESET);
+	}
+	mutex_unlock(&qsfp->lock);
+
+	schedule_delayed_work(&qsfp->dwork, msecs_to_jiffies(QSFP_CHECK_TIME));
+}
+
+int qsfp_init_work(struct qsfp *qsfp)
+{
+	qsfp->init = QSFP_INIT_RESET;
+	INIT_DELAYED_WORK(&qsfp->dwork, qsfp_check_hotplug);
+	qsfp_check_hotplug(&qsfp->dwork.work);
+	return 0;
+}
+EXPORT_SYMBOL_GPL(qsfp_init_work);
+
+int qsfp_register_regmap(struct qsfp *qsfp)
+{
+	struct device *dev = qsfp->dev;
+
+	qsfp->regmap = devm_regmap_init_mmio(dev, qsfp->base, &mmio_cfg);
+	if (IS_ERR(qsfp->regmap))
+		dev_err(dev, "Failed to create qsfp regmap\n");
+
+	return PTR_ERR_OR_ZERO(qsfp->regmap);
+}
+EXPORT_SYMBOL_GPL(qsfp_register_regmap);
+
+void qsfp_remove_device(struct qsfp *qsfp)
+{
+	writeq(CONF_MOD_SEL, qsfp->base + CONF_OFF);
+	cancel_delayed_work_sync(&qsfp->dwork);
+}
+EXPORT_SYMBOL_GPL(qsfp_remove_device);
+MODULE_LICENSE("GPL");
---
+++ drivers/net/phy/qsfp-mem-platform.c
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: GPL-2.0
+
+/* Intel(R) Memory based QSFP driver for platform devices.
+ *
+ * Copyright (C) 2022 Intel Corporation. All rights reserved.
+ */
+
+#include <linux/phy/qsfp-mem.h>
+#include <linux/bitfield.h>
+#include <linux/module.h>
+#include <linux/of_address.h>
+#include <linux/of_device.h>
+#include <linux/processor.h>
+#include <linux/slab.h>
+
+#define INTEL_QSFP_MEM_CONTROLLER_NAME "qsfp-mem-ctrl"
+
+static int qsfp_platform_probe(struct platform_device *pdev)
+{
+	struct device *dev = &pdev->dev;
+	struct resource *region = NULL;
+	struct resource *qsfpconfig = NULL;
+	struct qsfp *qsfp = NULL;
+	int ret = 0;
+
+	qsfp = devm_kzalloc(dev, sizeof(*qsfp), GFP_KERNEL);
+	if (!qsfp)
+		return -ENOMEM;
+
+	qsfp->dev = dev;
+	mutex_init(&qsfp->lock);
+	platform_set_drvdata(pdev, qsfp);
+
+	/* QSFP Mem address space */
+	qsfpconfig = platform_get_resource_byname(pdev, IORESOURCE_MEM,
+						  INTEL_QSFP_MEM_CONTROLLER_NAME);
+	if (!qsfpconfig) {
+		dev_err(dev, "resource %s not defined\n",
+			INTEL_QSFP_MEM_CONTROLLER_NAME);
+		return -ENODEV;
+	}
+
+	region = devm_request_mem_region(dev, qsfpconfig->start,
+					 resource_size(qsfpconfig), dev_name(dev));
+	if (!region) {
+		dev_err(dev, "unable to request %s\n", INTEL_QSFP_MEM_CONTROLLER_NAME);
+		return -EBUSY;
+	}
+	qsfp->base = devm_ioremap(dev, region->start, resource_size(region));
+	if (!(qsfp->base)) {
+		dev_err(dev, "ioremap of %s failed!", INTEL_QSFP_MEM_CONTROLLER_NAME);
+		return -ENOMEM;
+	}
+
+	ret = qsfp_init_work(qsfp);
+	if (ret != 0) {
+		dev_err(dev, "Failed to initialize delayed work to read QSFP\n");
+		return ret;
+	}
+
+	return qsfp_register_regmap(qsfp);
+}
+
+static int qsfp_platform_remove(struct platform_device *pdev)
+{
+	struct device *dev = &pdev->dev;
+	struct qsfp *qsfp = dev_get_drvdata(dev);
+
+	qsfp_remove_device(qsfp);
+	mutex_destroy(&qsfp->lock);
+	return 0;
+}
+
+static const struct of_device_id intel_fpga_qsfp_mem_ids[] = {
+	{ .compatible = "intel,qsfp-mem",
+		.data = NULL, },
+	{},
+};
+MODULE_DEVICE_TABLE(of, intel_fpga_qsfp_mem_ids);
+
+static struct platform_driver qsfp_driver = {
+	.probe      = qsfp_platform_probe,
+	.remove     = qsfp_platform_remove,
+	.suspend    = NULL,
+	.resume     = NULL,
+	.driver     = {
+		.name   = "intel,qsfp-mem",
+		.owner  = THIS_MODULE,
+		.of_match_table = intel_fpga_qsfp_mem_ids,
+	},
+};
+
+module_platform_driver(qsfp_driver);
+MODULE_DESCRIPTION("Intel(R) Memory based QSFP Platform driver");
+MODULE_AUTHOR("Intel Corporation");
+MODULE_LICENSE("GPL");
+
