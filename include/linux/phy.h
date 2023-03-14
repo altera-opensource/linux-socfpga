4f194f7dd2f246149979e78c0dd26360ac04b8c3
inianpavel <95082788+iniavanivan@users.noreply.github.com>
2023-03-14 15:06:59 +0530

    Phymodepatch (#55)
    * applied patch for missing 25gbase-kr mode
    
    * fixed conflict
    
    ---------
    
    Co-authored-by: Tham, Mun Yew <mun.yew.tham@intel.com>

--- include/linux/phy.h
+++ include/linux/phy.h
@@ -170,6 +170,8 @@ typedef enum {
 	PHY_INTERFACE_MODE_QUSGMII,
 	PHY_INTERFACE_MODE_1000BASEKX,
 	PHY_INTERFACE_MODE_10G_QXGMII,
+	/* 25GBASE-KR */
+	PHY_INTERFACE_MODE_25GKR,
 	PHY_INTERFACE_MODE_MAX,
 } phy_interface_t;
 
@@ -291,8 +293,13 @@ static inline const char *phy_modes(phy_interface_t interface)
 		return "100base-x";
 	case PHY_INTERFACE_MODE_QUSGMII:
 		return "qusgmii";
+<<<<<<< HEAD
 	case PHY_INTERFACE_MODE_10G_QXGMII:
 		return "10g-qxgmii";
+=======
+	case PHY_INTERFACE_MODE_25GKR:
+		return "25gbase-kr";
+>>>>>>> e6724c89ab45 (Phymodepatch (#55))
 	default:
 		return "unknown";
 	}
