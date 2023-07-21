4ebe2e7beae04acb14057a100dc63c708efb4440
Niravkumar L Rabara <niravkumar.l.rabara@intel.com>
2023-07-21 17:00:38 +0800

    HSD #15012127935: mtd: spi-nor: issi: add support for IS25WP01GG
    Add support for ISSI IS25WP01GG QSPI Flash.
    
    Signed-off-by: Niravkumar L Rabara <niravkumar.l.rabara@intel.com>

--- drivers/mtd/spi-nor/issi.c
+++ drivers/mtd/spi-nor/issi.c
@@ -64,6 +64,8 @@ static const struct flash_info issi_nor_parts[] = {
 		PARSE_SFDP
 		FIXUP_FLAGS(SPI_NOR_4B_OPCODES)
 		.fixups = &is25lp256_fixups },
+	{ "is25wp01",  INFO(0x9d7021, 0, 0, 0)
+		PARSE_SFDP },
 	{ "is25wp032",  INFO(0x9d7016, 0, 64 * 1024,  64)
 		NO_SFDP_FLAGS(SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
 	{ "is25wp064",  INFO(0x9d7017, 0, 64 * 1024, 128)
