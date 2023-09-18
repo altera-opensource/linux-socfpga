65ae12649c50159ff9ed12bed4487de958f603c1
Austin Zhang <austin.zhang@intel.com>
2023-09-18 13:07:50 +0800

    HSD #14015549956: USB: dwc3: Add 40 bit dma bit-mask quirk
    Agilex5 bus width is 40 bits, if bit mask is set to 64 bits the sent
    over the bus will be truncated.
    Add 40 bit dma bit-mask quirk to support Agilex5 SMMU.
    
    Signed-off-by: Adrian Ng Ho Yin <adrian.ho.yin.ng@intel.com>

--- drivers/usb/dwc3/core.h
+++ drivers/usb/dwc3/core.h
@@ -1157,6 +1157,7 @@ struct dwc3_scratchpad_array {
  * @suspended: set to track suspend event due to U3/L2.
  * @susphy_state: state of DWC3_GUSB2PHYCFG_SUSPHY + DWC3_GUSB3PIPECTL_SUSPHY
  *		  before PM suspend.
+ * @dma_set_40_bit_mask_quirk: set if we want to set dma bit mask to 40 bits.
  * @imod_interval: set the interrupt moderation interval in 250ns
  *			increments or 0 to disable.
  * @max_cfg_eps: current max number of IN eps used across all USB configs.
@@ -1390,6 +1391,7 @@ struct dwc3 {
 	unsigned		wakeup_configured:1;
 	unsigned		suspended:1;
 	unsigned		susphy_state:1;
+	unsigned		dma_set_40_bit_mask_quirk:1;
 
 	u16			imod_interval;
 
