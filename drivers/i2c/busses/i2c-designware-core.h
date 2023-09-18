29ba509f64390caaa357f2c99aebf1c5063e2068
Austin Zhang <austin.zhang@intel.com>
2023-09-19 03:09:10 +0800

    i2c: designware: introduce a custom scl recovery for SoCFPGA platforms
    [upstream commit b2f670af1feab3325cce5a251128f30711148771]
    
    The I2C pins on the SoCFPGA platforms do not go through a GPIO module,
    thus cannot be recovered by the default method of by doing a GPIO access.
    Only a reset of the I2C IP block can a recovery be successful, so this
    change effectively resets the I2C controller, NOT any attached clients.
    
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Dinh Nguyen <dinguyen@kernel.org>
    ---
    v6: Updated commit log to emphasize this change only resets the I2C
        controller and not any attached clients.
    v5: removed strayed nextline
    v4: re-arrange code per Andy Shevchenko's recommendation
    v3: simplify the function
        update commit message
    v2: remove change to MODEL_MASK
        s/i2c_custom_scl_recovery/i2c_socfpga_scl_recovery

--- drivers/i2c/busses/i2c-designware-core.h
+++ drivers/i2c/busses/i2c-designware-core.h
@@ -310,7 +310,8 @@ struct dw_i2c_dev {
 #define MODEL_BAIKAL_BT1			BIT(9)
 #define MODEL_AMD_NAVI_GPU			BIT(10)
 #define MODEL_WANGXUN_SP			BIT(11)
-#define MODEL_MASK				GENMASK(11, 8)
+#define MODEL_SOCFPGA				BIT(12)
+#define MODEL_MASK				GENMASK(12, 8)
 
 /*
  * Enable UCSI interrupt by writing 0xd at register
