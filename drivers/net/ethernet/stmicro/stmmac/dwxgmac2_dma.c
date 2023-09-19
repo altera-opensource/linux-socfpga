90ad8b45eb398a3af5d3841cd84c79bad0887292
Austin Zhang <austin.zhang@intel.com>
2023-09-20 02:26:07 +0800

    HSD #15012986938: net: stmmac: Use interrupt mode INTM=1 for per channel irq
    commit 6ccf12ae111e ("net: stmmac: use interrupt mode INTM=1
    for multi-MSI") is introduced for platform that uses MSI.
    
    Similar approach is taken to enable per channel interrupt
    that uses shared peripheral interrupt (SPI), so only per channel
    TX and RX intr (TI/RI) are handled by TX/RX ISR without calling
    common interrupt ISR.
    
    TX/RX NORMAL interrupts check is now decoupled, since NIS bit
    is not asserted for any TI/RI events when INTM=1.
    
    Signed-off-by: Teoh Ji Sheng <ji.sheng.teoh@intel.com>
    Signed-off-by: Khairul Anuar Romli <khairul.anuar.romli@altera.com>
        State: pending

--- drivers/net/ethernet/stmicro/stmmac/dwxgmac2_dma.c
+++ drivers/net/ethernet/stmicro/stmmac/dwxgmac2_dma.c
@@ -31,6 +31,13 @@ static void dwxgmac2_dma_init(void __iomem *ioaddr,
 		value |= XGMAC_EAME;
 
 	writel(value, ioaddr + XGMAC_DMA_SYSBUS_MODE);
+
+	if (dma_cfg->multi_irq_en) {
+		value = readl(ioaddr + XGMAC_DMA_MODE);
+		value &= ~DMA_MODE_INTM_MASK;
+		value |= (DMA_MODE_INTM_MODE1 << DMA_MODE_INTM_SHIFT);
+		writel(value, ioaddr + XGMAC_DMA_MODE);
+	}
 }
 
 static void dwxgmac2_dma_init_chan(struct stmmac_priv *priv,
--- include/linux/stmmac.h
+++ include/linux/stmmac.h
@@ -102,6 +102,7 @@ struct stmmac_dma_cfg {
 	bool aal;
 	bool eame;
 	bool multi_msi_en;
+	bool multi_irq_en;
 	bool dche;
 	bool atds;
 };
@@ -295,6 +296,11 @@ struct plat_stmmacenet_data {
 	u8 vlan_fail_q;
 	struct pci_dev *pdev;
 	int int_snapshot_num;
+	int ext_snapshot_num;
+	bool int_snapshot_en;
+	bool ext_snapshot_en;
+	bool multi_msi_en;
+	bool multi_irq_en;
 	int msi_mac_vec;
 	int msi_wol_vec;
 	int msi_lpi_vec;
