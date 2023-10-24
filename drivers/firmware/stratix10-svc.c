5988000dc188d48d13abf279d7d68061e8565b30
Adrian Ng Ho Yin <adrian.ho.yin.ng@intel.com>
2023-10-24 09:48:52 +0800

    HSD#15014543910: firmware:stratix10-svc: Fix Coverity warnings
    Changed ret variable type to int in allocate_memory function.
    Add jump statement to error handling during driver probe.
    
    Signed-off-by: Adrian Ng Ho Yin <adrian.ho.yin.ng@intel.com>

--- drivers/firmware/stratix10-svc.c
+++ drivers/firmware/stratix10-svc.c
@@ -1752,7 +1752,7 @@ void *stratix10_svc_allocate_memory(struct stratix10_svc_chan *chan,
 	struct gen_pool *genpool = chan->ctrl->genpool;
 	size_t s;
 	void *va;
-	unsigned int ret;
+	int ret;
 	struct iova *alloc;
 	dma_addr_t dma_addr;
 
@@ -1924,6 +1924,7 @@ static int stratix10_svc_drv_probe(struct platform_device *pdev)
 					pr_debug("Intel Service Layer Driver: IOVA cache failed\n");
 					iommu_domain_free(controller->domain);
 					ret = -ENODEV;
+					goto err_destroy_pool;
 				}
 				ret = iommu_attach_device(controller->domain, dev);
 				if (ret) {
