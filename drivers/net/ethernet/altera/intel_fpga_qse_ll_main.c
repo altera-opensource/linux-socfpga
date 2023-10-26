d8d8dc4b88312883eacdac439e851d4b5ac5bcc5
LCSwee <103010993+LCSwee@users.noreply.github.com>
2023-10-26 09:59:11 +0800

    HSD #15013714814: ethernet: altera: fix coverity issue (#160)
    fix coverity scan issue "var_deref_model: Passing null pointer
    priv->phylink to phylink_destroy, which dereferences it.
    
    Signed-off-by: Swee Leong Ching <leong.ching.swee@intel.com>

--- drivers/net/ethernet/altera/intel_fpga_qse_ll_main.c
+++ drivers/net/ethernet/altera/intel_fpga_qse_ll_main.c
@@ -1587,7 +1587,9 @@ static void intel_fpga_qse_ll_remove(struct platform_device *pdev)
 	if (priv->ptp_enable)
 		intel_fpga_tod_unregister(&priv->ptp_priv);
 
-	phylink_destroy(priv->phylink);
+	if (priv->phylink)
+		phylink_destroy(priv->phylink);
+
 	platform_set_drvdata(pdev, NULL);
 	unregister_netdev(ndev);
 	free_netdev(ndev);
