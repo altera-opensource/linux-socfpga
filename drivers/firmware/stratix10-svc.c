21613e27ad1721b2b54a09fce1b09549c6a5603d
Austin Zhang <austin.zhang@intel.com>
2024-04-02 04:42:39 +0800

    HSD#15015508123: firmware: stratix10-svc: fix timeout issue during fpga config
    Updated iommu domain allocation method.
    Updated iommu map memory attribute.
    Added dma_map_single call to flush data from cache to memory.
    Added svc layer api that returns size of payload sent to SDM.
    Added dma_unmap_single to unmap memory.
    
    Signed-off-by: Adrian Ng Ho Yin <adrian.ho.yin.ng@intel.com>
    
    State: pending

--- drivers/firmware/stratix10-svc.c
+++ drivers/firmware/stratix10-svc.c
@@ -2391,7 +2391,6 @@ int stratix10_svc_send(struct stratix10_svc_chan *chan, void *msg)
 				if (p_mem->vaddr == p_msg->payload) {
 					p_data->paddr = p_mem->paddr;
 					p_data->size = p_msg->payload_length;
-					break;
 				}
 			if (p_msg->payload_output) {
 				list_for_each_entry(p_mem, &svc_data_mem, node)
@@ -2507,7 +2506,8 @@ void *stratix10_svc_allocate_memory(struct stratix10_svc_chan *chan,
 		dma_addr = iova_dma_addr(&chan->ctrl->carveout.domain, alloc);
 
 		ret = iommu_map(chan->ctrl->domain, dma_addr, virt_to_phys(va),
-				s, IOMMU_READ | IOMMU_WRITE, GFP_KERNEL);
+				s, IOMMU_READ | IOMMU_WRITE | IOMMU_MMIO | IOMMU_CACHE,
+				GFP_KERNEL);
 		if (ret < 0) {
 			pr_debug("%s IOMMU map failed\n", __func__);
 			free_iova(&chan->ctrl->carveout.domain,
@@ -2651,7 +2651,7 @@ static int stratix10_svc_drv_probe(struct platform_device *pdev)
 		if (device_iommu_mapped(&pdev->dev)) {
 			controller->is_smmu_enabled = true;
 			pr_debug("Intel Service Layer Driver: IOMMU Present\n");
-			controller->domain = iommu_paging_domain_alloc(&pdev->dev);
+			controller->domain = iommu_get_dma_domain(dev);
 
 			if (!controller->domain) {
 				pr_debug("Intel Service Layer Driver: Error IOMMU domain\n");
