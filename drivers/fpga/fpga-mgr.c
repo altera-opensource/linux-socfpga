32d7021c16ab23989aceb2672060bf7fcabb9365
Adrian Ng Ho Yin <adrian.ho.yin.ng@intel.com>
2024-05-20 17:47:47 +0800

    HSD#15015929503 fpga: fpga_mgr: fix fpga_mgr_register not returning pointer to fpga_mgr
    Fix fpga_mgr_register not returning pointer to fpga_mgr causing kernel
    panic during stratix10-soc driver unload.
    
    Signed-off-by: Adrian Ng Ho Yin <adrian.ho.yin.ng@intel.com>

--- drivers/fpga/fpga-mgr.c
+++ drivers/fpga/fpga-mgr.c
@@ -833,6 +833,10 @@ fpga_mgr_register_full(struct device *parent, const struct fpga_manager_info *in
 		return ERR_PTR(ret);
 	}
 
+	fpga_mgr_debugfs_add(mgr);
+
+	dev_info(&mgr->dev, "%s registered\n", mgr->name);
+
 	return mgr;
 
 error_device:
