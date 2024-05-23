0209d58a534d88706193e88806182656bf769601
Adrian Ng Ho Yin <adrian.ho.yin.ng@intel.com>
2024-05-23 11:18:38 +0800

    HSD#15016115663 :firmware: stratix10-svc: fix build warnings
    Fix build warning for undescribed member in structure.
    
    Signed-off-by: Adrian Ng Ho Yin <adrian.ho.yin.ng@intel.com>
    
    State: pending
    Reported-by: kernel test robot <lkp@intel.com>

--- drivers/firmware/stratix10-svc.c
+++ drivers/firmware/stratix10-svc.c
@@ -275,6 +275,7 @@ struct stratix10_async_ctrl {
  * @sdm_lock: only allows a single command single response to SDM
  * @domain: pointer to allocated iommu domain
  * @is_smmu_enabled: flag to indicate whether is smmu_enabled for device
+ * @sdm_dma_addr_offset: dma addr offset to append to the IOVA sent to SDM
  * @carveout: iova_domain used to allocate iova addr that is accessible by SDM
  *
  * This struct is used to create communication channels for service clients, to
@@ -309,6 +310,9 @@ struct stratix10_svc_controller {
  * @name: service client name associated with the channel
  * @lock: protect access to the channel
  * @async_chan: reference to asynchronous channel object for this channel
+ * @task: pointer to the thread task which handles SMC or HVC call
+ * @svc_fifo: svc fifo circular buffer
+ * @svc_fifo_lock: svc fifo lock
  *
  * This struct is used by service client to communicate with service layer.
  * Each service client has its own channel created by service controller.
