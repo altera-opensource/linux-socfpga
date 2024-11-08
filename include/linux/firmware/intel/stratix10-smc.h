c38de41592892e76a10e4519832f1fbe1ca7693e
Mahesh Rao <mahesh.rao@intel.com>
2024-11-08 13:06:08 +0800

    HSD #15011993515-5: firmware: stratix10-svc: Add for SDM mailbox doorbell interrupt
    Add support for SDM mailbox doorbell interrupt
    async transactions. On interrupt , a workqueue is
    triggered which polls the ATF for pending responses
    and retrieves the bitmap of all transaction ids of
    the retrieved responses from SDM mailbox. It then
    triggers the corresponding registered callbacks.
    
    Signed-off-by: Mahesh Rao <mahesh.rao@intel.com>
    
    State: pending

--- include/linux/firmware/intel/stratix10-smc.h
+++ include/linux/firmware/intel/stratix10-smc.h
@@ -1992,4 +1992,24 @@ INTEL_SIP_SMC_FAST_CALL_VAL(INTEL_SIP_SMC_FUNCID_FPGA_CONFIG_COMPLETED_WRITE)
 #define INTEL_SIP_SMC_SDM_REMAPPER_CONFIG \
 	INTEL_SIP_SMC_FAST_CALL_VAL(INTEL_SIP_SMC_FUNCID_SDM_REMAPPER_CONFIG)
 
+/**
+ * Request INTEL_SIP_SMC_ASYNC_POLL_ON_IRQ
+ * Async call used by service driver at EL1 to query QSPI device info from FW
+ *
+ * Call register usage:
+ * a0 INTEL_SIP_SMC_ASYNC_POLL_ON_IRQ
+ * a1 transaction job id
+ * a2-7 will be used to return the response data
+ *
+ * Return status
+ * a0 INTEL_SIP_SMC_STATUS_OK
+ * a1-a4 will contain bitmap of available responses's transaction id as set bit position.
+ * a5-17 not used
+ * Or
+ * a0 INTEL_SIP_SMC_STATUS_NO_RESPONSE
+ * a1-17 not used
+ */
+#define INTEL_SIP_SMC_ASYNC_FUNC_ID_IRQ_POLL (0xC9)
+#define INTEL_SIP_SMC_ASYNC_POLL_ON_IRQ \
+	INTEL_SIP_SMC_ASYNC_VAL(INTEL_SIP_SMC_ASYNC_FUNC_ID_IRQ_POLL)
 #endif
