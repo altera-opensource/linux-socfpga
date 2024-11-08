9469327c771dbb95f1fefd9ad4ffb5416c5561ed
Mahesh Rao <mahesh.rao@intel.com>
2024-11-08 12:51:55 +0800

    HSD #15011993515-2: firmware: stratix10-svc: Implement ID pool management for asynchronous operations
    Implement ID pool management API's which will be
    used for Stratix10 Asynchronous communication with
    Secure Device Manager.
    
    Signed-off-by: Mahesh Rao <mahesh.rao@intel.com>
    
    State: pending

--- drivers/firmware/stratix10-svc.c
+++ drivers/firmware/stratix10-svc.c
@@ -331,6 +331,22 @@ struct stratix10_svc_chan {
 	struct stratix10_async_chan *async_chan;
 };
 
+/**
+ * struct stratix10_sip_id_pool - Structure representing a pool of IDs for
+ *                                asynchronous operations.
+ * @head:         The head index of the ID pool.
+ * @size:         The total size of the ID pool.
+ * @id_mask:      Pointer to an array representing the mask of allocated IDs.
+ * @lock:         Mutex lock to protect access to the ID pool.
+ */
+struct stratix10_sip_id_pool {
+	unsigned long head;
+	unsigned long size;
+	unsigned long *id_mask;
+	/* protects id pool*/
+	struct mutex lock;
+};
+
 static LIST_HEAD(svc_ctrl);
 static LIST_HEAD(svc_data_mem);
 
