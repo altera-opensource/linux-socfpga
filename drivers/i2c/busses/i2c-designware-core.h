120f8aa6fa410f74c75f39b273b083e8b595c175
tanmaykathpalia <tanmay.kathpalia@intel.com>
2025-03-11 13:40:43 +0530

    HSD #15016141215: i2c: designware: Fix SOCFPGA-specific recovery issue in MODEL_MASK (#221)
    After rebase to kernel 6.6.x, the MODEL_MASK macro did not include
    BIT 12, which is now assigned to MODEL_SOCFPGA due to the addition
    of a new model (WANGXUN_SP) that uses BIT 11. As a result, the
    SOCFPGA-specific I2C recovery function was not invoked, leading to
    issues such as the SCL line being stuck in some cases.
    
    Signed-off-by: Tanmay Kathpalia <tanmay.kathpalia@intel.com>

--- drivers/i2c/busses/i2c-designware-core.h
+++ drivers/i2c/busses/i2c-designware-core.h
@@ -309,7 +309,7 @@ struct dw_i2c_dev {
 #define MODEL_AMD_NAVI_GPU			BIT(10)
 #define MODEL_WANGXUN_SP			BIT(11)
 #define MODEL_SOCFPGA				BIT(12)
-#define MODEL_MASK				GENMASK(11, 8)
+#define MODEL_MASK				GENMASK(12, 8)
 
 /*
  * Enable UCSI interrupt by writing 0xd at register
