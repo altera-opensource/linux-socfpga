27166f30bf8211a120325ea2bbf493cd293e1cd1
Balsundar Ponnusamy <balsundar.ponnusamy@intel.com>
2025-05-15 18:27:14 +0800

    HSD#15017227419-11: misc: fcs_config: Added streaming APIs for mac verify
    Implemented streaming APIs for the hmac verification
    Added the attributes for init, update and final stage
    
    Signed-off-by: Khadgi Sagar <sagar.khadgi@intel.com>
    Signed-off-by: Balsundar Ponnusamy <balsundar.ponnusamy@intel.com>
        State: pending

--- drivers/misc/socfpga-config.c
+++ drivers/misc/socfpga-config.c
@@ -1478,6 +1478,96 @@ out:
 	return ret;
 }
 
+static ssize_t mac_verify_init_store(struct device *dev,
+				     struct device_attribute *attr,
+				     const char *buf, size_t buf_size)
+{
+	struct fcs_cmd_context *const u_ctx = *(struct fcs_cmd_context **)buf;
+	struct fcs_cmd_context *k_ctx;
+	int ret = buf_size;
+
+	k_ctx = hal_get_fcs_cmd_ctx();
+	if (!k_ctx) {
+		pr_err("Failed get context. Context is in use\n");
+		ret = -EFAULT;
+		goto out;
+	}
+
+	if (copy_from_user(k_ctx, u_ctx, sizeof(struct fcs_cmd_context))) {
+		pr_err("Failed to copy context from user space\n");
+		ret = -EFAULT;
+		goto out;
+	}
+
+	ret = hal_mac_verify_streaming_init(k_ctx);
+	if (ret)
+		pr_err("Failed to mac verify init\n");
+out:
+	hal_release_fcs_cmd_ctx(k_ctx);
+
+	return ret;
+}
+
+static ssize_t mac_verify_update_store(struct device *dev,
+				       struct device_attribute *attr,
+				       const char *buf, size_t buf_size)
+{
+	struct fcs_cmd_context *const u_ctx = *(struct fcs_cmd_context **)buf;
+	struct fcs_cmd_context *k_ctx;
+	int ret = buf_size;
+
+	k_ctx = hal_get_fcs_cmd_ctx();
+	if (!k_ctx) {
+		pr_err("Failed get context. Context is in use\n");
+		ret = -EFAULT;
+		goto out;
+	}
+
+	if (copy_from_user(k_ctx, u_ctx, sizeof(struct fcs_cmd_context))) {
+		pr_err("Failed to copy context from user space\n");
+		ret = -EFAULT;
+		goto out;
+	}
+
+	ret = hal_mac_verify_streaming_update(k_ctx);
+	if (ret)
+		pr_err("Failed to mac verify update\n");
+out:
+	hal_release_fcs_cmd_ctx(k_ctx);
+
+	return ret;
+}
+
+static ssize_t mac_verify_final_store(struct device *dev,
+				      struct device_attribute *attr,
+				      const char *buf, size_t buf_size)
+{
+	struct fcs_cmd_context *const u_ctx = *(struct fcs_cmd_context **)buf;
+	struct fcs_cmd_context *k_ctx;
+	int ret = buf_size;
+
+	k_ctx = hal_get_fcs_cmd_ctx();
+	if (!k_ctx) {
+		pr_err("Failed get context. Context is in use\n");
+		ret = -EFAULT;
+		goto out;
+	}
+
+	if (copy_from_user(k_ctx, u_ctx, sizeof(struct fcs_cmd_context))) {
+		pr_err("Failed to copy context from user space\n");
+		ret = -EFAULT;
+		goto out;
+	}
+
+	ret = hal_mac_verify_streaming_final(k_ctx);
+	if (ret)
+		pr_err("Failed to mac verify final\n");
+out:
+	hal_release_fcs_cmd_ctx(k_ctx);
+
+	return ret;
+}
+
 static DEVICE_ATTR_WO(open_session);
 static DEVICE_ATTR_WO(close_session);
 static DEVICE_ATTR_WO(context_info);
@@ -1529,6 +1619,9 @@ static DEVICE_ATTR_WO(get_digest_init);
 static DEVICE_ATTR_WO(get_digest_update);
 static DEVICE_ATTR_WO(get_digest_final);
 static DEVICE_ATTR_WO(get_digest);
+static DEVICE_ATTR_WO(mac_verify_init);
+static DEVICE_ATTR_WO(mac_verify_update);
+static DEVICE_ATTR_WO(mac_verify_final);
 
 static struct attribute *fcs_config_attrs[] = {
 	&dev_attr_open_session.attr,
@@ -1582,6 +1675,9 @@ static struct attribute *fcs_config_attrs[] = {
 	&dev_attr_get_digest_update.attr,
 	&dev_attr_get_digest_final.attr,
 	&dev_attr_get_digest.attr,
+	&dev_attr_mac_verify_init.attr,
+	&dev_attr_mac_verify_update.attr,
+	&dev_attr_mac_verify_final.attr,
 	NULL
 };
 
