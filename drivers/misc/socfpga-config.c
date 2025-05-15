ca440aef24976c4367a0bccfa57fe296610224e4
Balsundar Ponnusamy <balsundar.ponnusamy@intel.com>
2025-05-15 18:13:39 +0800

    HSD#15017227419-9: misc: fcs_config: message digest generation update
    Adding sysfs attributes for supporting message digest generation
    with streaming and one shot mode
    
    Signed-off-by: Khadgi Sagar <sagar.khadgi@intel.com>
    Signed-off-by: Balsundar Ponnusamy <balsundar.ponnusamy@intel.com>
        State: pending

--- drivers/misc/socfpga-config.c
+++ drivers/misc/socfpga-config.c
@@ -1363,6 +1363,121 @@ out:
 	return ret;
 }
 
+static ssize_t get_digest_init_store(struct device *dev,
+				     struct device_attribute *attr,
+				     const char *buf, size_t buf_size)
+{
+	struct fcs_cmd_context *const u_ctx = *(struct fcs_cmd_context **)buf;
+	struct fcs_cmd_context *k_ctx;
+	int ret;
+
+	k_ctx = hal_get_fcs_cmd_ctx();
+	if (!k_ctx) {
+		pr_err("Failed get context. Context is in use\n");
+		ret = -EFAULT;
+		goto out;
+	}
+
+	if (copy_from_user(k_ctx, u_ctx, sizeof(struct fcs_cmd_context))) {
+		pr_err("Failed to copy context from user space\n");
+		ret = -EFAULT;
+		goto out;
+	}
+
+	ret = hal_digest_streaming_init(k_ctx);
+	if (ret)
+		pr_err("Failed to perform get digest init\n");
+out:
+	hal_release_fcs_cmd_ctx(k_ctx);
+	return ret;
+}
+
+static ssize_t get_digest_update_store(struct device *dev,
+				       struct device_attribute *attr,
+				       const char *buf, size_t buf_size)
+{
+	struct fcs_cmd_context *const u_ctx = *(struct fcs_cmd_context **)buf;
+	struct fcs_cmd_context *k_ctx;
+	int ret;
+
+	k_ctx = hal_get_fcs_cmd_ctx();
+	if (!k_ctx) {
+		pr_err("Failed get context. Context is in use\n");
+		ret = -EFAULT;
+		goto out;
+	}
+
+	if (copy_from_user(k_ctx, u_ctx, sizeof(struct fcs_cmd_context))) {
+		pr_err("Failed to copy context from user space\n");
+		ret = -EFAULT;
+		goto out;
+	}
+
+	ret = hal_digest_streaming_update(k_ctx);
+	if (ret)
+		pr_err("Failed to perform get digest update\n");
+out:
+	hal_release_fcs_cmd_ctx(k_ctx);
+	return ret;
+}
+
+static ssize_t get_digest_final_store(struct device *dev,
+				      struct device_attribute *attr,
+				      const char *buf, size_t buf_size)
+{
+	struct fcs_cmd_context *const u_ctx = *(struct fcs_cmd_context **)buf;
+	struct fcs_cmd_context *k_ctx;
+	int ret;
+
+	k_ctx = hal_get_fcs_cmd_ctx();
+	if (!k_ctx) {
+		pr_err("Failed get context. Context is in use\n");
+		ret = -EFAULT;
+		goto out;
+	}
+	if (copy_from_user(k_ctx, u_ctx, sizeof(struct fcs_cmd_context))) {
+		pr_err("Failed to copy context from user space\n");
+		ret = -EFAULT;
+		goto out;
+	}
+
+	ret = hal_digest_streaming_final(k_ctx);
+	if (ret)
+		pr_err("Failed to perform get digest final\n");
+out:
+	hal_release_fcs_cmd_ctx(k_ctx);
+	return ret;
+}
+
+static ssize_t get_digest_store(struct device *dev,
+				struct device_attribute *attr, const char *buf,
+				size_t buf_size)
+{
+	struct fcs_cmd_context *const u_ctx = *(struct fcs_cmd_context **)buf;
+	struct fcs_cmd_context *k_ctx;
+	int ret;
+
+	k_ctx = hal_get_fcs_cmd_ctx();
+	if (!k_ctx) {
+		pr_err("Failed get context. Context is in use\n");
+		ret = -EFAULT;
+		goto out;
+	}
+
+	if (copy_from_user(k_ctx, u_ctx, sizeof(struct fcs_cmd_context))) {
+		pr_err("Failed to copy context from user space\n");
+		ret = -EFAULT;
+		goto out;
+	}
+
+	ret = hal_get_digest(k_ctx);
+	if (ret)
+		pr_err("Failed to perform get digest update\n");
+out:
+	hal_release_fcs_cmd_ctx(k_ctx);
+	return ret;
+}
+
 static DEVICE_ATTR_WO(open_session);
 static DEVICE_ATTR_WO(close_session);
 static DEVICE_ATTR_WO(context_info);
@@ -1410,6 +1525,10 @@ static DEVICE_ATTR_WO(ecdsa_data_sign_final);
 static DEVICE_ATTR_WO(ecdsa_data_verify_init);
 static DEVICE_ATTR_WO(ecdsa_data_verify_up);
 static DEVICE_ATTR_WO(ecdsa_data_verify_final);
+static DEVICE_ATTR_WO(get_digest_init);
+static DEVICE_ATTR_WO(get_digest_update);
+static DEVICE_ATTR_WO(get_digest_final);
+static DEVICE_ATTR_WO(get_digest);
 
 static struct attribute *fcs_config_attrs[] = {
 	&dev_attr_open_session.attr,
@@ -1459,6 +1578,10 @@ static struct attribute *fcs_config_attrs[] = {
 	&dev_attr_ecdsa_data_verify_init.attr,
 	&dev_attr_ecdsa_data_verify_up.attr,
 	&dev_attr_ecdsa_data_verify_final.attr,
+	&dev_attr_get_digest_init.attr,
+	&dev_attr_get_digest_update.attr,
+	&dev_attr_get_digest_final.attr,
+	&dev_attr_get_digest.attr,
 	NULL
 };
 
