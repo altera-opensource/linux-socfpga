f9d9763b5ca813b2b7da22ceb8bca04a916d1527
Adrian Ng Ho Yin <adrianhoyin.ng@altera.com>
2025-09-11 15:37:53 +0800

    HSD#22021259142: i3c: dw-i3c: Treat TX/RX length mismatch as M0 error
    Enhance error handling for Common Command Code (CCC) operations in the
    DesignWare I3C master driver:
    
    * Define GET_MRL_MIN_LEN (2) for validating GETMRL responses.
    * In dw_i3c_ccc_get(), treat an RX length smaller than the expected
      payload length or smaller than GET_MRL_MIN_LEN for GETMRL as an
      M0 (frame) error and return -EIO.
    * In dw_i3c_ccc_set(), similarly map RX length mismatches to I3C_ERROR_M0.
    
    These changes improve robustness by flagging incomplete or malformed
    responses during CCC transfers rather than silently accepting truncated
    data.
    
    Signed-off-by: Adrian Ng Ho Yin <adrianhoyin.ng@altera.com>
    
    State: pending

--- drivers/i3c/master/dw-i3c-master.c
+++ drivers/i3c/master/dw-i3c-master.c
@@ -243,6 +243,8 @@
 #define AMD_I3C_OD_PP_TIMING		BIT(1)
 #define DW_I3C_DISABLE_RUNTIME_PM_QUIRK	BIT(2)
 
+#define GET_MRL_MIN_LEN 2
+
 struct dw_i3c_cmd {
 	u32 cmd_lo;
 	u32 cmd_hi;
@@ -760,7 +762,8 @@ static int dw_i3c_ccc_set(struct dw_i3c_master *master,
 			xfer->cmds[0].error == RESPONSE_ERROR_ADDRESS_NACK)
 		ccc->err = I3C_ERROR_M2;
 
-	if (xfer->cmds[0].error == RESPONSE_ERROR_FRAME)
+	if (xfer->cmds[0].error == RESPONSE_ERROR_FRAME ||
+			(cmd->rx_len < ccc->dests[0].payload.len))
 		ccc->err = I3C_ERROR_M0;
 
 	return ret;
@@ -770,6 +773,7 @@ static int dw_i3c_ccc_get(struct dw_i3c_master *master, struct i3c_ccc_cmd *ccc)
 {
 	struct dw_i3c_cmd *cmd;
 	int ret, pos;
+	bool rx_len_mismatch;
 
 	pos = dw_i3c_master_get_addr_pos(master, ccc->dests[0].addr);
 	if (pos < 0)
@@ -803,8 +807,15 @@ static int dw_i3c_ccc_get(struct dw_i3c_master *master, struct i3c_ccc_cmd *ccc)
 			xfer->cmds[0].error == RESPONSE_ERROR_ADDRESS_NACK)
 		ccc->err = I3C_ERROR_M2;
 
-	if (xfer->cmds[0].error == RESPONSE_ERROR_FRAME)
+	if (ccc->id == I3C_CCC_GETMRL)
+		rx_len_mismatch = (cmd->rx_len < GET_MRL_MIN_LEN);
+	else
+		rx_len_mismatch = (cmd->rx_len < ccc->dests[0].payload.len);
+
+	if (xfer->cmds[0].error == RESPONSE_ERROR_FRAME || rx_len_mismatch) {
 		ccc->err = I3C_ERROR_M0;
+		ret = -EIO;
+	}
 
 	return ret;
 }
