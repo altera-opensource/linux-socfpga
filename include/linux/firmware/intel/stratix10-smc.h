a1718b2e2b76f3c793e6e830ae5233004df38fd5
Mahesh Rao <mahesh.rao@altera.com>
2025-10-27 22:54:41 +0800

    firmware: stratix10-svc: Add support for async communication
    Introduce support for asynchronous communication with the Stratix10
    service channel. Define new structures to enable asynchronous messaging
    with the Secure Device Manager (SDM). Add and remove asynchronous
    support for existing channels. Implement initialization and cleanup
    routines for the asynchronous framework. Enable sending and polling of
    messages to the SDM asynchronously.
    
    The new public functions added are:
    - stratix10_svc_add_async_client: Adds a client to the service channel.
    - stratix10_svc_remove_async_client: Removes an asynchronous client from
            the service channel.
    - stratix10_svc_async_send: Sends an asynchronous message to the SDM
            mailbox in EL3 secure firmware.
    - stratix10_svc_async_poll: Polls the status of an asynchronous service
            request in EL3 secure firmware.
    - stratix10_svc_async_done: Marks an asynchronous transaction as
            complete and frees up the resources.
    
    These changes enhance the functionality of the Stratix10 service channel
    by allowing for more efficient and flexible communication with the
    firmware.
    
    Signed-off-by: Mahesh Rao <mahesh.rao@altera.com>
    Reviewed-by: Matthew Gerlach <matthew.gerlach@altera.com>
    Signed-off-by: Dinh Nguyen <dinguyen@kernel.org>

--- include/linux/firmware/intel/stratix10-smc.h
+++ include/linux/firmware/intel/stratix10-smc.h
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
  * Copyright (C) 2017-2018, Intel Corporation
+ * Copyright (C) 2025, Altera Corporation
  */
 
 #ifndef __STRATIX10_SMC_H
@@ -46,6 +47,10 @@
 #define INTEL_SIP_SMC_FAST_CALL_VAL(func_num) \
 	ARM_SMCCC_CALL_VAL(ARM_SMCCC_FAST_CALL, ARM_SMCCC_SMC_64, \
 	ARM_SMCCC_OWNER_SIP, (func_num))
+
+#define INTEL_SIP_SMC_ASYNC_VAL(func_name)	\
+	ARM_SMCCC_CALL_VAL(ARM_SMCCC_STD_CALL, ARM_SMCCC_SMC_64, \
+	ARM_SMCCC_OWNER_SIP, (func_name))
 
 /**
  * Return values in INTEL_SIP_SMC_* call
@@ -620,4 +625,58 @@ INTEL_SIP_SMC_FAST_CALL_VAL(INTEL_SIP_SMC_FUNCID_FPGA_CONFIG_COMPLETED_WRITE)
 #define INTEL_SIP_SMC_FCS_GET_PROVISION_DATA \
 	INTEL_SIP_SMC_FAST_CALL_VAL(INTEL_SIP_SMC_FUNCID_FCS_GET_PROVISION_DATA)
 
+/**
+ * Request INTEL_SIP_SMC_HWMON_READTEMP
+ * Sync call to request temperature
+ *
+ * Call register usage:
+ * a0 Temperature Channel
+ * a1-a7 not used
+ *
+ * Return status
+ * a0 INTEL_SIP_SMC_STATUS_OK
+ * a1 Temperature Value
+ * a2-a3 not used
+ */
+#define INTEL_SIP_SMC_FUNCID_HWMON_READTEMP 32
+#define INTEL_SIP_SMC_HWMON_READTEMP \
+	INTEL_SIP_SMC_FAST_CALL_VAL(INTEL_SIP_SMC_FUNCID_HWMON_READTEMP)
+
+/**
+ * Request INTEL_SIP_SMC_HWMON_READVOLT
+ * Sync call to request voltage
+ *
+ * Call register usage:
+ * a0 Voltage Channel
+ * a1-a7 not used
+ *
+ * Return status
+ * a0 INTEL_SIP_SMC_STATUS_OK
+ * a1 Voltage Value
+ * a2-a3 not used
+ */
+#define INTEL_SIP_SMC_FUNCID_HWMON_READVOLT 33
+#define INTEL_SIP_SMC_HWMON_READVOLT \
+	INTEL_SIP_SMC_FAST_CALL_VAL(INTEL_SIP_SMC_FUNCID_HWMON_READVOLT)
+
+/**
+ * Request INTEL_SIP_SMC_ASYNC_POLL
+ * Async call used by service driver at EL1 to query mailbox response from SDM.
+ *
+ * Call register usage:
+ * a0 INTEL_SIP_SMC_ASYNC_POLL
+ * a1 transaction job id
+ * a2-17 will be used to return the response data
+ *
+ * Return status
+ * a0 INTEL_SIP_SMC_STATUS_OK
+ * a1-17 will contain the response values from mailbox for the previous send
+ * transaction
+ * Or
+ * a0 INTEL_SIP_SMC_STATUS_NO_RESPONSE
+ * a1-17 not used
+ */
+#define INTEL_SIP_SMC_ASYNC_FUNC_ID_POLL (0xC8)
+#define INTEL_SIP_SMC_ASYNC_POLL \
+	INTEL_SIP_SMC_ASYNC_VAL(INTEL_SIP_SMC_ASYNC_FUNC_ID_POLL)
 #endif
