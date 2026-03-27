282a160504f2dd435a067c67c07e7ab4f53be58e
Subhransu S. Prusty <subhransu.sekhar.prusty@altera.com>
2026-03-27 16:02:28 +0530

    HSD #18044206347: net: ethernet: altera: Fix return type of msgdma_pref_tx_buffer
    Fix warning: conflicting types for ‘msgdma_pref_tx_buffer’. Return type
    expected netdev_tx_t, but it was int.
    
    Signed-off-by: Subhransu S. Prusty <subhransu.sekhar.prusty@altera.com>

--- drivers/net/ethernet/altera/altera_msgdma_prefetcher.h
+++ drivers/net/ethernet/altera/altera_msgdma_prefetcher.h
@@ -27,7 +27,7 @@ void msgdma_pref_clear_txirq(struct altera_dma_private *priv);
 u32 msgdma_pref_tx_completions(struct altera_dma_private *priv);
 void msgdma_pref_add_rx_desc(struct altera_dma_private *priv,
 			     struct altera_dma_buffer *buffer);
-int msgdma_pref_tx_buffer(struct altera_dma_private *priv,
+netdev_tx_t msgdma_pref_tx_buffer(struct altera_dma_private *priv,
 			  struct altera_dma_buffer *buffer);
 u32 msgdma_pref_rx_status(struct altera_dma_private *priv);
 int msgdma_pref_initialize(struct altera_dma_private *priv);
